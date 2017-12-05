2b26bdadc4cfc28fde20ca0cd26639644739822f
Your Name <you@example.com>
2017-12-05 09:46:32 +0100

    allow workflow names to have names with spaces. May be I will regret it ;-)

--- EMadmin/src/create_proj/urls.py
+++ EMadmin/src/create_proj/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^add_acquisition$', views.add_acquisition, name='add_acquisition'),
     url(r'^add_acquisition2$', views.add_acquisition2, name='add_acquisition2'),
-    url(r'^getWorkflow/(?P<name>\w{0,50})/', views.getWorkflow, name='getWorkflow'),
+    url(r'^getWorkflow/(?P<name>[\w\ ]{0,128})/', views.getWorkflow, name='getWorkflow'),
 ]
