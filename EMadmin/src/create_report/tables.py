4f07417e282290293ee6cde40a2827c60b6d78bf
scipion user <scipionuser@scipionbox.cnb.csic.es>
2018-01-05 11:18:21 +0100

    add id to tables

--- EMadmin/src/create_report/tables.py
+++ EMadmin/src/create_report/tables.py
@@ -4,9 +4,12 @@ from create_proj.models import Acquisition
 from django.conf import settings
 
 class ProjectsTable(tables.Table):
+    session_id = tables.TemplateColumn( '{{record.id}}', order_by=('projname'))
+
     go_to_Html_Summary = tables.TemplateColumn('<a href="http://%s/%s/{{record.projname}}">'
                                      '{{record.projname}}</a>'%
                                      (settings.PUBLISHURL, settings.PUBLISHUSER), order_by=('projname'))
+
     get_Report = tables.TemplateColumn('<a href={% url "create_report:create_report_latex" record.id %}>'
                                        '{{record.projname}}</a>',
                                        order_by=('projname'))
