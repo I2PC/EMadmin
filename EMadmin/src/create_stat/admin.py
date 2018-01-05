eaddca6ad3678568d1d5d82d5195c568c40d2c30
scipion user <scipionuser@scipionbox.cnb.csic.es>
2018-01-05 11:19:01 +0100

    modify admin view

--- EMadmin/src/create_stat/admin.py
+++ EMadmin/src/create_stat/admin.py
@@ -4,5 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from models import Statistics
 
+class StatisticsAdmin(admin.ModelAdmin):
+    list_display = ('acquisition','numberMovies', 'averageResolution')
+
 # Register your models here.
-admin.site.register(Statistics)
+admin.site.register(Statistics,StatisticsAdmin )
+
+
