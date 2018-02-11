6852c501bb7a049740a1f5e44efec19758b1b80d
rmarabini <roberto@cnb.csic.es>
2018-02-11 23:46:25 +0100

    add latex stuff for invoce

--- EMadmin/src/invoice/views.py
+++ EMadmin/src/invoice/views.py
@@ -2,25 +2,195 @@
 from __future__ import unicode_literals
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
-from forms import InvoiceForm1, InvoiceForm2, InvoiceForm3
+from forms import InvoiceForm1, InvoiceForm2
+from django.contrib.auth import get_user_model
 # Create your views here.
 from create_proj.models import Acquisition
+from models import Invoice, InvoiceLine, Concept
+import jinja2, os
+from django.conf import settings
+import unidecode
+from django.http import HttpResponse
+
+User = get_user_model()
+
+
+def pretty_request(request):
+    headers = ''
+    for header, value in request.META.items():
+        if not header.startswith('HTTP'):
+            continue
+        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+        headers += '{}: {}\n'.format(header, value)
+
+    return (
+        '{method} HTTP/1.1\n'
+        'Content-Length: {content_length}\n'
+        'Content-Type: {content_type}\n'
+        '{headers}\n\n'
+        '{body}'
+    ).format(
+        method=request.method,
+        content_length=request.META['CONTENT_LENGTH'],
+        content_type=request.META['CONTENT_TYPE'],
+        headers=headers,
+        body=request.body,
+)
+
+
+import re
+
+def tex_escape(text):
+    """
+        :param text: a plain text message
+        :return: the message escaped to appear correctly in LaTeX
+    """
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless ',
+        '>': r'\textgreater ',
+    }
+    regex = re.compile('|'.join(re.escape(unicode(key)) for key in sorted(conv.keys(), key = lambda item: - len(item))))
+    return regex.sub(lambda match: conv[match.group()], text)
+
+def create_report_latex(invoice):
+    # init template procesor jinja2 for
+    latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('/'))
+    )
+
+    latexTemplateFile = settings.LATEX_INVOICE_TEMPLATE
+    latexLogoFile = settings.LATEX_REPORT_TEMPLATE_ICON
+    template = latex_jinja_env.get_template(os.path.realpath(latexTemplateFile))
+
+    options = {}
+    # Header
+    acquisition = invoice.acquisition
+    options['projectName'] = tex_escape(acquisition.projname)
+    options['invoicenum'] = acquisition.id
+    options['companyname'] = settings.COMPNAME
+    orderedBy = invoice.ordered_by.name + "\\\\" + \
+                invoice.ordered_by.profile.institution
+    options['orderedby'] = orderedBy
+    concepts=""
+#%    My product 1 & 15\texteuro & 10 & 150\texteuro\\
+#%    My product 2 & 25\texteuro & 10 & 250\texteuro\\
+#%    My product 3 & 35\texteuro & 10 & 350\texteuro\\
+#%    My product 4 & 45\texteuro & 10 & 450\texteuro\\
+
+
+
+    for line in invoice.items.all():
+        concepts += "%s & %8.2f\\texteuro & %d & %8.2f\\texteuro\\\\"%(
+            line.concept.name,
+                                              line.unit_price,
+                                    line.quantity, line.total())
+    options['concepts'] = concepts
+    options['total'] = invoice.total()
+
+    # if project exists compile data in scipionuserdata/project/Logs
+    # otherwise use /tmp
+    out_dir=os.path.join(settings.SCIPIONUSERDATA, "projects",
+                         invoice.acquisition.projname, "Tmp")
+    if os.path.isdir(out_dir):
+        pass
+    else:
+        out_dir = "/tmp"
+    out_file_root=os.path.join(out_dir,"temp")
+    renderer_template = template.render(**options)
+    with open(out_file_root + ".tex", "w") as f:  # saves tex_code to outpout file
+        f.write(unidecode.unidecode(renderer_template))
+
+    os.system('pdflatex -output-directory %s %s'%(out_dir, out_file_root))
+    with open(os.path.join(out_dir, out_file_root + ".pdf"), 'r') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        response['Content-Disposition'] = 'inline;filename=some_file.pdf'
+        return response
+    pdf.close()
+    return HttpResponse("Can not create invoice")
+
 
 @staff_member_required
-
-
 def create_invoice(request, idacquisition):
-    #acquisition = Acquisition.objects.get(pk=idacquisition)
+    acquisition = Acquisition.objects.get(pk=idacquisition)
     if request.method == 'POST':
-        form = InvoiceForm1(request)
-        if form.is_valid():
-            pass
+        print pretty_request(request)
+        form1 = InvoiceForm1(request.POST)
+        form2 = InvoiceForm2(request.POST)
+        if form1.is_valid() and form2.is_valid():
+            # process form1
+            data1 = form1.cleaned_data
+            user = data1['orderedBy']
+            # check if there is an invoice for this adquisition
+            # if there is update it
+            invoice, created = Invoice.objects.get_or_create(
+                                          acquisition=acquisition
+                                          )
+            invoice.ordered_by = user
+            invoice.save()
+            # process form2
+            data2 = form2.cleaned_data
+            conceptDict={}
+            quantityDict={}
+            for k,v in data2.iteritems():
+                key = int(k[2:])
+
+                if k[0]=='n':
+                    v = int(v)
+                    if v == 0:
+                        continue
+                    quantityDict[key] = v
+                elif k[0]=='c':
+                    if v.name == "-----":
+                        continue
+                    conceptDict[key] = v
+
+            for k,v in   conceptDict.iteritems():
+                invoiceline, created = InvoiceLine.objects.get_or_create(
+                        invoice=invoice,
+                        concept=v)
+                invoiceline.quantity = quantityDict[k]
+                invoiceline.unit_price = v.unit_price
+                invoiceline.save()
+            return create_report_latex(invoice)
         else:
             pass
     else:
-        form1 = InvoiceForm1(auto_id=False)
-        form2 = InvoiceForm2(auto_id=False)
-        form3 = InvoiceForm3(auto_id=False)
-    return render(request, 'invoice/invoice.html', {'form1': form1,
+        # check if there is an invoice for this project
+        # if it does exists do not allow modifications
+        # infor of ID.
+        invoices = Invoice.objects.filter(acquisition=acquisition)
+        if invoices.exists():
+            invoice = invoices[0]
+            orderedBy = invoice.ordered_by.id
+        else:
+            invoice = None
+            orderedBy = acquisition.user.id
+        form1 = InvoiceForm1(auto_id=False, orderedBy=acquisition.user.id)
+        form2 = InvoiceForm2(auto_id=False, invoice=invoice)
+    return render(request, 'invoice/invoice.html', {'projname':
+                                                        acquisition.projname,
+                                                    'acquisition':
+                                                        acquisition.id,
+                                                    'form1': form1,
                                                     'form2': form2,
-                                                    'form3': form3})
+                                                    })
