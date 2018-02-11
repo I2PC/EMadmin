7cd302e7c01f6f80c648450af39c12826fe60c9e
rmarabini <roberto@cnb.csic.es>
2018-02-11 23:45:58 +0100

    remove unused url

--- EMadmin/src/invoice/urls.py
+++ EMadmin/src/invoice/urls.py
@@ -4,6 +4,6 @@ import views
 urlpatterns = [
     url(r'^create_invoice/(?P<idacquisition>[\w\-]+)/$', views.create_invoice,
         name='create_invoice'),
-    url(r'^create_invoice/$', views.create_invoice,
-        name='create_invoice'),
+#    url(r'^create_invoice/$', views.create_invoice,
+#        name='create_invoice'),
 ]
