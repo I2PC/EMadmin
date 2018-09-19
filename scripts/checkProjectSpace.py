ccb63a898f6763a1944b59186e78fe8a50904196
rmarabini <roberto@cnb.csic.es>
2018-09-19 22:10:27 +0200

    first version

--- scripts/checkProjectSpace.py
+++ scripts/checkProjectSpace.py
@@ -6,13 +6,20 @@ PROJECT_HOME = '/home/scipionuser/OffloadData'
 TABULIST = ['2018_06_25_foo_wwqeqweqw/', '2018_06_18_foo_ribosome/']
 DBNAME=''
 # get list of project directories
+def _print(list, msg):
+    for item in list:
+        print msg, item
+
 def checkProjectDirectory():
     directoryList = glob("%s/2???_??_??_*/" % PROJECT_HOME)
+    _print(directoryList, "checkProjectDirectory")
     return directoryList
 
 # filter through tabu
 def applyTabuDirectory(directoryList):
-    return [item for item in directoryList if item not in TABULIST]
+    directoryList = [item for item in directoryList if item not in TABULIST]
+    _print(applyTabuDirectory, "checkProjectDirectory")
+    return directoryList
 
 # get emails from database
 def getEmails(directoryList):
