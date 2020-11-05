d60066918745783bbf32c73f1c1015f1f04ef6a8
Your Name <you@example.com>
2020-11-05 19:39:46 +0100

    set log directory

--- EMadmin/src/EMadmin/settings/development.py
+++ EMadmin/src/EMadmin/settings/development.py
@@ -32,7 +32,8 @@ INTERNAL_IPS = [
 ]
 
 # Log everything to the logs directory at the top
-LOGFILE_ROOT = join(dirname(BASE_DIR), 'logs')
+#ROB: LOGFILE_ROOT = join(dirname(BASE_DIR), 'logs')
+LOGFILE_ROOT = '/home/scipionuser/logs'
 
 # Reset logging
 # (see http://www.caktusgroup.com/blog/2015/01/27/Django-Logging-Configuration-logging_config-default-settings-logger/)
